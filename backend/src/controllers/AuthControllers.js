f8ca26985d915e0902f8479b09a94f20eae7d60c
devCaiodias <caiodiasmartins12@gmail.com>
2025-08-05 14:45:58 -0300

    corrigindo erro em produção de Token ausente

--- backend/src/controllers/AuthControllers.js
+++ backend/src/controllers/AuthControllers.js
@@ -61,7 +61,7 @@ class AuthController {
            res.cookie('token', token, {
                 httpOnly: true,
                 secure: true,
-                sameSite: 'lax',
+                sameSite: 'None',
                 maxAge: 7 * 24 * 60 * 60 * 1000,
             });
 
